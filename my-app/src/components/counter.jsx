ce936f487c9c0437cd07ea36939d0ea04db368ae
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-17 00:01:51 +0530

    render classes dynamically

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -5,15 +5,28 @@ class Counter extends Component {
     count: 0,
     // imageUrl: "https://picsum.photos/200",
   };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
         {/* <img src={this.state.imageUrl}></img> */}
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     //return count === 0 ? <h1>Zero</h1> : count;
