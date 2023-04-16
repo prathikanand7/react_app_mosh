82caf290f213ce4c9494a0778ed681f47562bcb7
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-17 00:07:38 +0530

    how to render lists in view

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
   state = {
     count: 0,
     // imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
@@ -18,6 +19,11 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
