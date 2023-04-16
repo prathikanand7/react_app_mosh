c1d62d7194002a6a327cb4a465b4a25b4f0594ec
Prathik Anand Krishnan <prathik.anandkrishnan@arup.com>
2023-04-17 00:20:17 +0530

    rendering lists - tags in view

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -11,19 +11,27 @@ class Counter extends Component {
     fontSize: 10,
     fontWeight: "bold",
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <div>
         {/* <img src={this.state.imageUrl}></img> */}
-        <span style={this.styles} className={this.getBadgeClasses()}>
+        {/* <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
   }
